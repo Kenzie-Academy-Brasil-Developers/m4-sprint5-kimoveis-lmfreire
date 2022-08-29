3fe9dd251113b353363a92ff957f0906aee6ffa3
lmfreire <l.matheus.freire10@gmil.com>
2022-08-29 20:40:29 -0300

    feat: done

---
+++ src/routes/schedulesRouter.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import createschedulesController from "../controlller/schedules/createschedulesController";
+import listchedulesController from "../controlller/schedules/listchedulesController";
+import AuthMiddleware from "../Middlewares/AuthMiddleware";
+import isAdmMiddleware from "../Middlewares/isAdmMiddlewares";
+
+const schedulesRouter = Router();
+
+schedulesRouter.post(
+  "",
+  AuthMiddleware,
+  isAdmMiddleware,
+  createschedulesController
+);
+schedulesRouter.get(
+  "/properties/:id",
+  AuthMiddleware,
+  isAdmMiddleware,
+  listchedulesController
+);
+
+export default schedulesRouter;
